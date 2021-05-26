9174c03c20bcd10dc866e9973f57a7fce851945b
AJMalcolm <aj-malcolm@hotmail.com>
2021-05-26 23:07:51 +0100

    Implemented the core functionality of the application, passed all unit tests at this stage with 100% test coverage. Corrected a few errors in the declaration of variables within the StringOutputBuilder class. Also amended the unit tests due to oversights (i.e. input being a multiple of more than 1 of 3,5,7).

--- src/main/java/malcolm/alasdair/raindropsutil/StringOutputBuilder.java
+++ src/main/java/malcolm/alasdair/raindropsutil/StringOutputBuilder.java
@@ -4,19 +4,33 @@ import static java.lang.Integer.parseInt;
 
 public class StringOutputBuilder {
 
-    private static String output;
-    private static int input;
+    private String output = "";
+    private int input;
 
     public StringOutputBuilder(String input) {
         this.input = parseInt(input);
         createOutput();
     }
 
-    private static void createOutput(){
-        //TODO: Core functionality occurs here
+    public StringOutputBuilder createOutput(){
+        if(input % 3 == 0){
+            output = output + "Pling";
+        }
+        if(input % 5 == 0){
+            output = output + "Plang";
+        }
+        if(input % 7 == 0){
+            output = output + "Plong";
+        }
+
+        if(output.isEmpty()){
+            output = Integer.toString(input);
+        }
+
+        return this;
     }
 
-    public static String getOutput() {
-        return output;
+    public String getOutput() {
+        return this.output;
     }
 }
--- src/test/java/malcolm/alasdair/unit/RaindropsUnitTests.java
+++ src/test/java/malcolm/alasdair/unit/RaindropsUnitTests.java
@@ -1,10 +1,7 @@
 package malcolm.alasdair.unit;
 
 import malcolm.alasdair.raindrops.Raindrops;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -22,21 +19,21 @@ public class RaindropsUnitTests {
         }
 
         @ParameterizedTest
-        @ValueSource(strings = {"3", "6", "9", "12", "15"})
+        @ValueSource(strings = {"3", "6", "9", "12", "18"})
         @DisplayName("raindrops returns 'Pling' for multiples of 3")
         void raindropsReturnsPlingForMultipleOfThree(String input) {
             Assertions.assertEquals("Pling", Raindrops.raindrops(input));
         }
 
         @ParameterizedTest
-        @ValueSource(strings = {"5", "10", "15", "20", "25"})
+        @ValueSource(strings = {"5", "10", "20", "25", "40"})
         @DisplayName("raindrops returns 'Plang' for multiples of 5")
         void raindropsReturnsPlangForMultipleOfFive(String input) {
             Assertions.assertEquals("Plang", Raindrops.raindrops(input));
         }
 
         @ParameterizedTest
-        @ValueSource(strings = {"7", "14", "21", "28", "35"})
+        @ValueSource(strings = {"7", "14", "28", "49", "56"})
         @DisplayName("raindrops returns 'Plong' for multiples of 7")
         void raindropsReturnsPlongForMultipleOfSeven(String input) {
             Assertions.assertEquals("Plong", Raindrops.raindrops(input));
